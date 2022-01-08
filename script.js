83b0b5731084ec89a88e4e67788616445870a57a
Dieter <dieterlotz2@gmail.com>
2022-01-08 12:14:00 +0200

    Added toggle via keyboard

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-console.log("Script loaded");
+const leftArrowKey = 'ArrowLeft';
+const rightArrowKey = 'ArrowRight';
 
 function toggle(){
     const monthlyPrices = Array.from(document.getElementsByName('monthlyPrice'));
     const annualPrices = Array.from(document.getElementsByName('annualPrice')); 
-    const checked = document.getElementById('switch').checked;
-   
-    if(!checked)
-    {
-        monthlyPrices.forEach(element => {
-            element.style.display = 'initial';
-         });
- 
-        annualPrices.forEach(element => {
-         element.style.display = 'none';
-         });
-
-         return;
-    }
+    const checkbox = document.getElementById('switch');
     
-    monthlyPrices.forEach(element => {
+    if(monthlyPrices[0].style.display === 'initial'){
+        checkbox.checked = true;
+
+        annualPrices.forEach(element => {
+            element.style.display = 'initial';
+        });
+
+        monthlyPrices.forEach(element => {
+            element.style.display = 'none';
+        });
+
+        return;
+    }
+
+    checkbox.checked = false;
+
+    annualPrices.forEach(element => {
         element.style.display = 'none';
     });
 
-    annualPrices.forEach(element => {
-    element.style.display = 'initial';
+    monthlyPrices.forEach(element => {
+        element.style.display = 'initial';
     });
 }
+
+document.addEventListener('keydown', (event) => {
+    if(event.defaultPrevented)
+        return;
+
+    if(event.key === leftArrowKey || event.key === rightArrowKey)
+        toggle();
+})
